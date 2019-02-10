b3d4688bb7c57a8dd71b0fac737dbc943f048f0b
Rjack00 <rjackson77@comcast.net>
2019-02-10 12:26:45 -0700

    created variable to require companies.js & called it with res.json in app.get

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express')
+const companies = require('./companies.json')
 const app = express()
 const port = 3000
 
 // app.get('/', (req, res) => res.json({"message": "Hello World!"}))
 
 //The below line replaces the above commented out line to send a file.
-app.get('/', (req, res) => res.sendFile(companies.json))
+app.get('/', (req, res) => res.json({companies}))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
