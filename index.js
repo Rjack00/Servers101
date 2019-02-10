95e7d8cc7ca2c3ca05a26fcbd2791adbfc570ac7
Rjack00 <rjackson77@comcast.net>
2019-02-09 21:44:30 -0700

    pasted csv converted to json in companies.json & changed index.js file app.get line to send companies.json

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.json({"message": "Hello World!"}))
+// app.get('/', (req, res) => res.json({"message": "Hello World!"}))
+
+//The below line replaces the above commented out line to send a file.
+app.get('/', (req, res) => res.sendFile(companies.json))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
