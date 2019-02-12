b6c8c28404279baaf5a80c0a9579a99819348f73
Rjack00 <rjackson77@comcast.net>
2019-02-11 20:07:35 -0700

    added findById funtion to use in app.get request to try to call a single item by the 'id' key

--- index.js
+++ index.js
@@ -3,9 +3,29 @@ const companies = require('./companies.json')
 const app = express()
 const port = 3000
 
-// app.get('/', (req, res) => res.json({"message": "Hello World!"}))
+app.get('/', (req, res) => res.json({ companies }))
 
-//The below line replaces the above commented out line to send a file.
-app.get('/', (req, res) => res.json({companies}))
+function findById(data, id) {
+    for (let i = 0; i < data.length; i++){
+        if (data[i].id == id){
+            return data[i];
+        }
+    }
+}
+
+app.get('/:id', function(req, res){
+    const record = findById(req.params.id);
+    if(!record){
+        res.status = 404;
+        res.json({
+            error: {
+                message: "No record found!"
+            }
+        });
+        }
+    res.json({data: record})
+});
+
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
