2173db88fe56e592e8aa91e7372fb3e68c9fb849
Rjack00 <rjackson77@comcast.net>
2019-03-07 14:58:59 -0700

    required CORS in index.js

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express')
+const cors = require('cors')
 const companies = require('./companies.json')
 const app = express()
 const port = process.env.PORT || 3000
+
+///////// CORS ////////////////////////
+
+app.use(cors())
+
+
+///////////////////////////////////////
 
 app.get('/', (req, res) => res.json({ companies }))
 
